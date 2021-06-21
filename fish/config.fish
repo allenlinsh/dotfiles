0f74b537e2c004a5e109a5b3480617a4ee25f702
Allen Lin <allenlinsh@gmail.com>
2021-06-22 07:32:50 +0800

    set up zathura pdf viewer

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,8 @@
-# bobthefish powerline
+# bobthefish powerline setup
 set -g theme_display_cmd_duration yes
 set -g theme_nerd_fonts yes
 set -g theme_git_default_branches main
 set -g theme_newline_cursor no
+
+# xkbcommon disable warning
+set -x XKB_LOG_LEVEL critical
