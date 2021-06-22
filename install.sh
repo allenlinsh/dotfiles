40ca06b7cfbe1f7c3b3ac900c8b1d0fe98b20b82
Allen Lin <allenlinsh@gmail.com>
2021-06-21 20:27:11 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,21 @@
-#!/bin/sh
-# Bash script for installing all the necessary packages and plugins
-# Run with 'sudo' if permission is denied
-# install packages
-apt update
-apt install neovim
-apt install fish
-apt install nodejs
+#!/usr/bin/sh
+
+# check sudo permission
+if [ "$EUID" -ne 0 ]
+  then sudo su
+fi
+
+# install prerequisites
+apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
+
+# instal packages
+apt update && apt install -y fish nodejs latexmk yarn
+yarn global add neovim prettier
 
 # install docker 
-apt install apt-transport-https ca-certificates curl gnupg lsb-release
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian (lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get install docker-ce
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian (lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt install -y docker-ce
 
 # install oh-my-fish
 curl -L https://get.oh-my.fish | fish
@@ -19,3 +23,5 @@ curl -L https://get.oh-my.fish | fish
 # install bobthefish powerline
 omf install bobthefish
 
+# manually install delta
+# https://github.com/dandavison/delta/releases/latest
