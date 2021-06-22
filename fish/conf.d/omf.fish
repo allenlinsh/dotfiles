7789a02af01c9b0dd88930b27267eacc257d131f
Allen Lin <allenlinsh@gmail.com>
2021-06-22 09:59:12 +0800

    Add tmux config

--- fish/conf.d/omf.fish
+++ fish/conf.d/omf.fish
@@ -1,7 +1,13 @@
-# Path to Oh My Fish install.
+# Path to Oh My Fish install
 set -q XDG_DATA_HOME
   and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
   or set -gx OMF_PATH "$HOME/.local/share/omf"
 
-# Load Oh My Fish configuration.
+# Load Oh My Fish configuration
 source $OMF_PATH/init.fish
+
+# Load tmux on startup
+if status is-interactive
+and not set -q TMUX
+    exec tmux -f ~/.config/tmux/tmux.conf
+end
