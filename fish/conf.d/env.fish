5ad6dfbebe69324de78f89549a80f084ef7af891
Allen Lin <allenlinsh@gmail.com>
2021-06-24 20:32:19 +0800

    Include texlive paths and reconfigure fish colors

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -1,1 +1,4 @@
-# golang
+# texlive
+set MANPATH /usr/local/texlive/2021/texmf-dist/doc/man $MANPATH
+set INFOPATH /usr/local/texlive/2021/texmf-dist/doc/info $INFOPATH
+set PATH /usr/local/texlive/2021/bin/x86_64-linux $PATH
--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,9 @@ set -g theme_git_default_branches main
 set -g theme_newline_cursor no
 set -g theme_color_scheme dracula
 
+# autosuggestion setup
+set -g fish_pager_color_description 'FFC042'
+
 # xkbcommon disable warning
 set -x XKB_LOG_LEVEL critical
 
