cbd9a0738f515c3c29433c8767d5571cdcb3472a
Allen Lin <allenlinsh@gmail.com>
2021-07-06 01:21:45 +0800

    Add jdk path

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -4,4 +4,5 @@ set INFOPATH /usr/local/texlive/2021/texmf-dist/doc/info $INFOPATH
 set PATH /usr/local/texlive/2021/bin/x86_64-linux $PATH
 
 # java
-set JAVA_PATH /usr/lib/jvm/java-1.11.0-openjdk-amd64/bin/java
+set JAVA_PATH /usr/lib/jvm/java-1.11.0-openjdk-amd64/bin/java $JAVA_PATH
+set PATH $JAVA_PATH/bin $PATH
